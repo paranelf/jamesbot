13e3d70f98672048b6e3b659d8a79f9c60dc5640
paranelf <56554575+paranelf@users.noreply.github.com>
2020-05-04 23:47:34 +0900

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,26 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game = discord.Game("RainbowSix Siege")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("잘가"):
+        await message.channel.send("안녕히계세요 ㅠㅠ")
+    if message.content.startswith("하이"):
+        await message.channel.send("안녕하세요!!")
+    if message.content.startswith("태양만세!!"):
+        await message.channel.send("\\\[T]/")
+
+
+
+
+client.run("NzA2NzM5MTU2ODE0MTM1MzA3.XrAquQ.0hNA0uN8uZWicnFboguUz-B5o-k")
